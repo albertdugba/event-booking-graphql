5c591ba5cbc4510758a34b009dce17f6a296f6a4
Albert Dugba <albert.dugba@gmail.com>
2019-08-29 17:18:41 +0000

    GraphQL set up

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const events = [];
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+
+    type Event{
+      _id:ID!
+      title:String!
+      description:String!
+      price:Float!
+      date:String!
+    }
+
+    input EventInput{
+      title:String!
+      description:String!
+      price:Float!
+      date:String!
+
+   }
+    type RootQuery{
+      events:[Event]!
+      
+    },
+    type RootMutation{
+      createEvent(eventInput:EventInput):Event
+
+    }
+
+    schema{
+        query:RootQuery 
+        mutation:RootMutation  
+    } 
+
+    `),
+    rootValue: {
+      events: args => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
+
+const PORT = 5000;
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-sayil.mongodb.net/test?retryWrites=true&w=majority`
+  )
+  .then(
+    app.listen(PORT, () => console.log(`Database is connected on port:${PORT}`))
+  )
+  .catch(err => console.log(err));
